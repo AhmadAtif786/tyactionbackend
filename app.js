4f2800e88adcf25302cbd6df54fa1ead11b55e42
MinahilNadeem123 <92431326+MinahilNadeem123@users.noreply.github.com>
2023-10-26 22:26:53 +0500

    josn

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const userRoutes = require("./routes/user");
+const userRoutes = require("/routes/user");
 const videoRoutes = require("./routes/video");
 
 const app = express();
