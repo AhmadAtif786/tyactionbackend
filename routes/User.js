836734c14a1abf77f987af5eddc4f03477e99c4e
MinahilNadeem123 <92431326+MinahilNadeem123@users.noreply.github.com>
2023-10-28 01:57:01 +0500

    josn

--- routes/User.js
+++ routes/User.js
@@ -2,44 +2,38 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const multer = require('multer');
-const path = require('path');
 const AWS = require('aws-sdk');
 
-const storage = multer.diskStorage({
-  destination: "uploads/",
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-    cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
-  },
-});
-
-const s3 = new AWS.S3();
-const s3BucketName = 'cyclic-shy-blue-mussel-robe-ap-northeast-2';
-
+// Set up Multer for file uploads (video thumbnails and resumes)
+const storage = multer.memoryStorage();
 const upload = multer({ storage });
+
+// Configure AWS SDK
+const s3 = new AWS.S3({
+  accessKeyId: 'ASIAYC2RJDOW6G26F3VW',
+  secretAccessKey: 'owJFL1g9w9yXTqAdlkdYTit3l4JpeMDdrxlWkKbF',
+  region: 'ap-northeast-2' // Set your preferred AWS region
+});
 
 // Create a new user
 router.post("/users", upload.single('image'), async (req, res) => {
   try {
     const { name, Lname, bio, description, pinnedSocialLinks, resumeLink, email } = req.body;
 
-    // Validate email format
-    if (!isValidEmail(email)) {
-      return res.status(400).json({ error: "Invalid email format" });
-    }
-
     let image = null;
 
     if (req.file) {
+      // Upload the file to AWS S3
       const fileData = req.file;
       const params = {
-        Bucket: s3BucketName,
-        Key: fileData.filename,
+        Bucket: 'cyclic-shy-blue-mussel-robe-ap-northeast-2',
+        Key: fileData.originalname, // Set the S3 key (filename)
         Body: fileData.buffer,
       };
 
       const s3UploadResponse = await s3.upload(params).promise();
-      image = s3UploadResponse.Key;
+
+      image = s3UploadResponse.Location; // Store the S3 file URL in the User model
     }
 
     const existingUser = await User.findOne({ email });
@@ -62,8 +56,7 @@ router.post("/users", upload.single('image'), async (req, res) => {
     await user.save();
     res.status(201).json(user);
   } catch (error) {
-    console.error(error); // Log the error for debugging
-    res.status(400).json({ error: "An error occurred while processing your request" });
+    res.status(400).json({ error: error.message });
   }
 });
 
@@ -71,10 +64,6 @@ router.get("/users", async (req, res) => {
   const email = req.query.email;
 
   try {
-    if (!isValidEmail(email)) {
-      return res.status(400).json({ error: "Invalid email format" });
-    }
-
     const user = await User.findOne({ email });
     if (!user) {
       return res.status(404).json({ error: "User not found" });
@@ -82,15 +71,8 @@ router.get("/users", async (req, res) => {
 
     res.json(user);
   } catch (error) {
-    console.error(error); // Log the error for debugging
-    res.status(500).json({ error: "An error occurred while processing your request" });
+    res.status(500).json({ error: error.message });
   }
 });
-
-// Simple email format validation
-function isValidEmail(email) {
-  const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
-  return emailRegex.test(email);
-}
 
 module.exports = router;
